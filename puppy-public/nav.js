6358cca020520300d6e182395541261aa34102fc
Bailey Lockheart <baileylockheart@gmail.com>
2025-01-20 19:25:51 -0800

    Updated navigation + new blog post!

---
+++ puppy-public/nav.js
@@ -0,0 +1,7 @@
+document.getElementById("nav").innerHTML = `
+<ul>
+    <li><a href='/blog/index.html'>Blog</a></li>
+    <li><a href='/stuff.html'>Stuff</a></li>
+    <li><a href='/links.html'>Links</a></li>
+</ul>
+`;
